e5c91ab6550faac4e9045eac1ad175cf680d4aa7
ovesh malpura <malpura09hunter@gmail.com>
2026-09-05 09:56:21 +0530

    Remove conflicting legacy College renderer

--- oveshcloud/college-structured-v4.js
+++
@@ -1,4 +0,0 @@
-(()=>{'use strict';
-/* Legacy College v4 renderer intentionally disabled. college-live-fix.js is the single authoritative College file explorer. */
-window.dispatchEvent(new CustomEvent('ovesh-college-legacy-disabled'));
-})();
